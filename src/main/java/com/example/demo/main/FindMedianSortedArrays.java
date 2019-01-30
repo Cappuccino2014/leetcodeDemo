0f13cc109e3646cd7b4c08df370b29565f270614
wenbo <liwenbo@admobgeek.com>
2019-01-30 20:31:37 +0800

    2

---
+++ src/main/java/com/example/demo/main/FindMedianSortedArrays.java
@@ -0,0 +1,16 @@
+package com.example.demo.main;
+
+/**
+ * @Auther: wilbur
+ * @Date: 2019/1/30 19:51
+ * @Description:
+ */
+public class FindMedianSortedArrays {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/example/demo/main/LengthOfLongestSubstring.java
+++ src/main/java/com/example/demo/main/LengthOfLongestSubstring.java
@@ -1,16 +1,135 @@
 package com.example.demo.main;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @Auther: wilbur
  * @Date: 2019/1/29 11:38
  * @Description:
  */
 public class LengthOfLongestSubstring {
+    /**
+     * 方法一：暴力法
+     * 思路
+     *
+     * 逐个检查所有的子字符串，看它是否不含有重复的字符。
+     * 假设我们有一个函数 boolean allUnique(String substring) ，如果子字符串中的字符都是唯一的，它会返回true，否则会返回false。
+     * 我们可以遍历给定字符串 s 的所有可能的子字符串并调用函数 allUnique。 如果事实证明返回值为true，那么我们将会更新无重复字符子串
+     * 的最大长度的答案。
+     * @param s
+     * @return
+     */
     public int lengthOfLongestSubstring(String s) {
-        return 1;
+        int n = s.length();
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                if (allUnique(s, i, j)) {
+                    result = Math.max(result, j - i);
+                }
+            }
+
+        }
+
+        return result;
+    }
+
+    public boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch)) return false;
+            set.add(ch);
+        }
+        return true;
+    }
+
+    /**
+     * 方法二：滑动窗口
+     *滑动窗口是数组/字符串问题中常用的抽象概念。 窗口通常是在数组/字符串中由开始和结束索引定义的一系列元素的集合，即 [i, j)[i,j)
+     * （左闭，右开）。而滑动窗口是可以将两个边界向某一方向“滑动”的窗口。例如，我们将 [i, j)[i,j) 向右滑动 11 个元素，则它将变为
+     * [i+1, j+1)[i+1,j+1)（左闭，右开）。
+     *
+     * 回到我们的问题，我们使用 HashSet 将字符存储在当前窗口 [i, j)[i,j)（最初 j = ij=i）中。 然后我们向右侧滑动索引 jj，如果它不
+     * 在 HashSet 中，我们会继续滑动 jj。直到 s[j] 已经存在于 HashSet 中。此时，我们找到的没有重复字符的最长子字符串将会以索引 ii
+     * 开头。如果我们对所有的 ii 这样做，就可以得到答案。
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring1(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            System.out.printf("%d,%d\n",i,j);
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 方法三：优化的滑动窗口
+     * 上述的方法最多需要执行 2n 个步骤。事实上，它可以被进一步优化为仅需要 n 个步骤。我们可以定义字符到索引的映射，而不是使用集合来
+     * 判断一个字符是否存在。 当我们找到重复的字符时，我们可以立即跳过该窗口。
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring2(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            System.out.printf("%d,%d\n",i,j);
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+
+    /**
+     * Java（假设字符集为 ASCII 128）
+     *
+     * 以前的我们都没有对字符串 s 所使用的字符集进行假设。
+     *
+     * 当我们知道该字符集比较小的时侯，我们可以用一个整数数组作为直接访问表来替换 Map。
+     *
+     * 常用的表如下所示：
+     *
+     * int [26] 用于字母 ‘a’ - ‘z’或 ‘A’ - ‘Z’
+     * int [128] 用于ASCII码
+     * int [256] 用于扩展ASCII码
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring3(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            System.out.printf("%d,%d\n",i,j);
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
-
+        String s = "abcabcbb";
+        LengthOfLongestSubstring lengthOfLongestSubstring = new LengthOfLongestSubstring();
+        int i = lengthOfLongestSubstring.lengthOfLongestSubstring3(s);
+        System.out.println(i);
     }
 }
